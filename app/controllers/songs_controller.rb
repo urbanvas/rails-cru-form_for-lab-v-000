522e3c754be6c5fdf46a888df017784364a972d9
Neil Seejoor <nvseejoor@gmail.com>
2018-07-20 19:46:34 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+    def artist_params
+    end
 end
